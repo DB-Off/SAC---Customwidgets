a13afaf22f66dc967c58cf768938466be7fa4c7d
DB-Off <bhaarathi.rk@gmail.com>
2025-08-13 10:41:43 +0530

    box.js

--- box/box.js
+++ box/box.js
@@ -1,58 +1,42 @@
 (function() { 
 	let template = document.createElement("template");
-	template.innerHTML = `
+	template.innerHTML = 
 		<style>
-		.box {
+		:host {
 			border-radius: 25px;
 			border-width: 4px;
 			border-color: black;
 			border-style: solid;
 			display: block;
-   			background-color: red; 
-			opacity: 1; 
 		} 
 		</style> 
-  		<div class="box"></div>
-	`;
+	;
 
 	class Box extends HTMLElement {
 		constructor() {
 			super(); 
-			this._shadowRoot = this.attachShadow({ mode: "open" });
-			this._shadowRoot.appendChild(template.content.cloneNode(true));
-			this.color = "#3498db"; // default color
-			this.opacity = 1; // default color
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+			});
 			this._props = {};
 		}
-		 setColor(newColor) {
-			 this.color = newColor;
-			 this._shadowRoot.querySelector(".box").style.backgroundColor = newColor;
-	}
-
-	 getColor() {
-		 return this.color;
-	}
 
 		onCustomWidgetBeforeUpdate(changedProperties) {
 			this._props = { ...this._props, ...changedProperties };
 		}
 
 		onCustomWidgetAfterUpdate(changedProperties) {
-			if (changedProperties.includes("color") || changedProperties.includes("opacity")) {
-				this._shadowRoot.querySelector(".box").style.backgroundColor = this.hexToRgba(this.color, this.opacity);
+			if ("color" in changedProperties) {
+				this.style["background-color"] = changedProperties["color"];
 			}
-			
+			if ("opacity" in changedProperties) {
+				this.style["opacity"] = changedProperties["opacity"];
+			}
 		}
 	}
 
 	customElements.define("com-sample-box", Box);
 })();
-
-
-
-
-
-
-
-
-
